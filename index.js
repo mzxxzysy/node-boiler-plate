f1a1347a69a9cb256cc9c1612ab1972814a4285a
mzxxzysy <jsyjj21@gmail.com>
2025-01-08 16:41:58 +0900

    2차

--- index.js
+++ index.js
@@ -5,17 +5,42 @@ const app = express();
 const port = 5002;
 //포트 번호 설정
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded 처럼 생긴 데이터를 가져오는 코드
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 으로 된 데이터를 가져오는 코드
+app.use(bodyParser.json());
+
 //몽고디비 연결하기
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://jsy:root1234!!@boiler-plate.luuuy.mongodb.net/?retryWrites=true&w=majority&appName=boiler-plate", {
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("mongodb connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world!"));
+app.get("/", (req, res) => res.send("Hello world!"));
 //root 디렉토리에 오면 hello world 출력
+
+app.post("/register", async (req, res) => {
+  //뢰원 가입 할 때 필요한 정보들을 client를 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  try {
+    const doc = await user.save();
+    return res.status(200).json({ success: true, data: doc });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
+});
+
 app.listen(port, () => console.log(`example app listening on port ${port}!`));
 //해당 포트 번호에서 실행
